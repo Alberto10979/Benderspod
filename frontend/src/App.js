8ab328c2bdbf5419affac1d9747b7471c30b18b0
alberto <alberto@albertos-MacBook-Air.local>
2025-08-06 03:54:40 +0300

    Added About page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Prediction from './Prediction';
+import About from './About';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/prediction" element={<Prediction />} />
+        <Route path="/about" element={<About/>}/>
       </Routes>
 
     </Router>
