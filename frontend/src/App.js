61898a7e8fc6d7c4f0682201ef37f324f824a217
alberto <alberto@albertos-MacBook-Air.local>
2025-08-06 04:16:37 +0300

    Added HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Prediction from './Prediction';
 import About from './About';
+import Homepage from './Homepage';
 
 function App() {
   return (
     <Router>
       <Routes>
+         <Route path="/" element={<Homepage />} />
+
         <Route path="/prediction" element={<Prediction />} />
         <Route path="/about" element={<About/>}/>
       </Routes>
