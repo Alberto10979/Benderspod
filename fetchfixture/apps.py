7126b7fb5e442f0525cafce09c8e5ef35c478a27
alberto <alberto@albertos-MacBook-Air.local>
2025-08-15 13:31:10 +0300

    new backend project

--- fetchfixture/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FetchfixtureConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fetchfixture'
--- fetchfixture/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('fixtures/upcoming/', views.upcoming_matches, name='upcoming_fixtures'),
-]
