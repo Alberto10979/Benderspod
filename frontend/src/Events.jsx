b29221f7efa933b6603eb098b22c245afd0d073b
alberto <alberto@albertos-MacBook-Air.local>
2025-08-16 05:00:28 +0300

    Adjusted api for Evants.jsx

--- frontend/src/Events.jsx
+++ frontend/src/Events.jsx
@@ -12,7 +12,7 @@ const Events = () => {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get('https://benderspod-production-1776.up.railway.app/api/events/all');
+        const response = await axios.get('https://benderspod-production-1776.up.railway.app/api/events/all/');
         setEvents(response.data);
         setLoading(false);
       } catch (err) {
