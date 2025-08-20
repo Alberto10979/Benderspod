9e248de826bcd7650e6ffae8ac55d0583047dc6b
alberto <alberto@albertos-MacBook-Air.local>
2025-08-20 12:52:41 +0300

    Changed serializers.py file

--- events/serializers.py
+++ events/serializers.py
@@ -1,20 +1,22 @@
 from rest_framework import serializers
 from .models import Event
-from django.conf import settings
 
 class EventSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
     
     class Meta:
         model = Event
-        fields = ['id', 'title', 'description', 'location', 'image_url']
+        fields = ['id', 'title', 'description', 'location', 'image_url', 'created_at', 'updated_at']
     
     def get_image_url(self, obj):
         if obj.image:
             try:
-                # This will work with Cloudinary
+                # This will work with both local files and cloud storage
+                request = self.context.get('request')
+                if request:
+                    return request.build_absolute_uri(obj.image.url)
                 return obj.image.url
             except:
-                # Fallback if image doesn't exist
+                # Fallback if image doesn't exist or URL generation fails
                 return None
         return None
