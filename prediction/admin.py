0280678a9b877514c6a5c4bf2d5fe2b7d45b0afb
alberto <alberto@albertos-MacBook-Air.local>
2025-08-21 15:58:56 +0300

    Adjusted admin.py for prediction

--- prediction/admin.py
+++ prediction/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Team, Player, Fixture, Prediction
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Prediction)
+
+@admin.register(Fixture)
+class FixtureAdmin(admin.ModelAdmin):
+    list_display = ('home_team', 'away_team', 'kickoff_at', 'is_open', 'home_score', 'away_score', 'man_of_the_match')
+    list_filter = ('is_open',)
+    autocomplete_fields = ('home_team', 'away_team', 'man_of_the_match')
+
